42b6f679fc4a8b459586ead19fc4de72b738e6f9
Ilya Kasyanenko <villiakas@gmail.com>
2021-12-12 13:19:19 +0300

    6.4. Слово начинается с ... [#41585 #238936]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -1,12 +1,9 @@
 package ru.job4j.array;
 
 public class ArrayChar {
-
     public static boolean startsWith(char[] word, char[] pref) {
         for (int i = 0; i < pref.length; i++) {
-            if (i > word.length - 1) {
-                return false;
-            } else if (pref[i] != word[i]) {
+            if (pref[i] != word[i]) {
                 return false;
             }
         }
