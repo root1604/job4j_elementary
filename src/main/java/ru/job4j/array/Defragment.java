3848b2652230240bbc69a5bf402e4be9d8480ede
Ilya Kasyanenko <villiakas@gmail.com>
2021-12-18 11:29:37 +0300

    6.8. Дефрагментация массива [#225]

---
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null) {
+                for (int j = 0; j < array.length; j++) {
+                    if (array[j] == null) {
+                        SwitchArray.swap(array, i, j);
+                        break;
+                    }
+                }
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (String s : compressed) {
+            System.out.print(s + " ");
+        }
+    }
+}
--- src/test/java/ru/job4j/array/SwitchArrayTest.java
+++ src/test/java/ru/job4j/array/SwitchArrayTest.java
@@ -23,4 +23,24 @@ public class SwitchArrayTest {
         int[] expected = {1, 3, 2, 4};
         Assert.assertArrayEquals(expected, result);
     }
+
+    @Test
+    public void whenSwapZeroToThree() {
+        String[] input = {"one", "two", "three", "four"};
+        int source = 0;
+        int dest = input.length - 1;
+        String[] result = SwitchArray.swap(input, source, dest);
+        String[] expected = {"four", "two", "three", "one"};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenSwapTwoToOne() {
+        String[] input = {"one", "two", "three", "four"};
+        int source = 2;
+        int dest = 1;
+        String[] result = SwitchArray.swap(input, source, dest);
+        String[] expected = {"one", "three", "two", "four"};
+        Assert.assertArrayEquals(expected, result);
+    }
 }
